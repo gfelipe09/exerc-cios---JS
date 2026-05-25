3411b09132abd037cfc6637e640dccd9e042dd77
I1HSA <menuzzobiel@icloud.com>
2026-05-25 16:16:56 -0300

    adicionei o sistema de contagem de pessoas e idades como no exercício da turma, ainda não está 100% completo, faltando complementos como IsNaN e os casos de palavras no singular

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
-let idade = Number(prompt("Insira a sua idade: "));
+let pessoas = Number(prompt("Insira a quantidade de pessoas: "))
+let criancas = 0
+let adolescentes = 0
+let jovens = 0
+let adultos = 0
+let idosos = 0
+let horaExtra = 0
 
-if (isNaN(idade)) {
+if (isNaN(pessoas)) {
 
     alert("Você não digitou um número!");
 
 }
 
 else {
+    for (let i = 0; i < pessoas; i++) {
 
-    if (idade < 12) {
+        let idade = Number(prompt("Insira a(s) idade(s): "));
 
-        alert("Criança!");
+        if (idade < 12) {
 
+            criancas = criancas + 1
+
+        }
+        else if (idade < 17) {
+
+            adolescentes = adolescentes + 1
+
+        }
+        else if (idade < 21) {
+
+            jovens = jovens + 1
+
+        }
+        else if (idade < 60) {
+
+            adultos = adultos + 1
+
+        }
+        else if (idade < 99) {
+
+            idosos = idosos + 1
+
+        }
+        else if (idade > 99) {
+
+            horaExtra = horaExtra + 1
+
+        }
     }
-    else if (idade < 17) {
 
-        alert("Adolescente!");
-
-    }
-    else if (idade < 21) {
-
-        alert("Jovem!");
-
-    }
-    else if (idade < 60) {
-
-        alert("Adulto!");
-
-    }
-    else if (idade < 99) {
-
-        alert("Idoso!");
-
-    }
-    else if (idade > 99) {
-
-        alert("Hora Extra!");
-
-    }
+    alert(`Ao todo, há ${pessoas} pessoas, havendo: ${criancas} crianças, ${adolescentes} adolescentes, ${jovens} jovens, ${adultos} adultos, ${idosos} idosos e ${horaExtra} fazendo hora extra.`);
 
 };
